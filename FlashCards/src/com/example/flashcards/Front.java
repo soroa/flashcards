b8e32578e5f3a429b6ead654c7da95278a4ed109
Michael <purgham@student.ethz.ch>
2013-04-24 16:04:57 +0200

    new version problem with git

---
+++ FlashCards/src/com/example/flashcards/Front.java
@@ -0,0 +1,93 @@
+package com.example.flashcards;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+
+import android.view.GestureDetector.OnGestureListener;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.GestureDetector;
+import android.widget.TextView;
+
+public class Front extends Activity  {
+
+	
+	//0: right, 1:up, 2:left, 3: down
+	private short swipe_h;
+	private short swipe_v;
+
+		
+	//hallo test
+	
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_front);
+		
+	
+	}
+	
+	@Override
+	public boolean onTouchEvent (MotionEvent event){
+		
+		
+		return gd.onTouchEvent(event);
+	}
+	
+	SimpleOnGestureListener simpleonGestureListener = new SimpleOnGestureListener(){
+		@Override
+		  public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+		    float velocityY) {
+		   
+		   float sensitvity = 50;
+		   
+		   // TODO Auto-generated method stub
+		   //right to left
+		   if((e1.getX() - e2.getX()) > sensitvity){
+		   
+		    showBack();
+		    swipe_h = 0;//left
+		   }else if((e2.getX() - e1.getX()) > sensitvity){
+		    
+		    swipe_h = 1;//right
+		    
+		   }else{
+		    swipe_h = -1;
+		   }
+		   
+		   if((e1.getY() - e2.getY()) > sensitvity){
+		    
+		    swipe_v= 0;//down
+		   }else if((e2.getY() - e1.getY()) > sensitvity){
+		    swipe_v = 1;//up
+		   }else{
+		    swipe_v += -1;
+		   }
+		   
+		   
+		   
+		   return super.onFling(e1, e2, velocityX, velocityY);
+		  }
+		   };
+
+	
+			GestureDetector gd = new GestureDetector(simpleonGestureListener);
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.front, menu);
+		return true;
+	}
+	//external function cuz I can't use the intent constructor with this from within the listener
+	public void showBack(){
+		Intent i = new Intent(this, Back.class);
+		startActivity(i);
+		
+	}
+
+}
---
+++ FlashCards/src/com/example/flashcards/Word.java
@@ -0,0 +1,104 @@
+package com.example.flashcards;
+
+import java.sql.Date;
+import java.util.Calendar;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.ContentValues;
+
+public class Word {
+	
+	public static final String MYDATABASE_NAME = "Flashcard_DATABASE";
+	public static final String MYDATABASE_LIBRARY = "Flashcard_LIBRARY";
+	public static final int MYDATABASE_VERSION = 1;
+	public static final String KEY_ID = "_id";
+	public static final String KEY_QUESTION = "question";
+	public static final String KEY_SOLUTION = "solution";
+	public static final String KEY_MISTAKES = "Numb_of_m";
+	//public static final Calendar KEY_DATE = Calendar.getInstance();
+	
+	private static final String SCRIPT_CREATE_DATABASE = "create table "
+			+ MYDATABASE_LIBRARY + " (" 
+			+ KEY_ID + " integer primary key autoincrement, " 
+			+ KEY_QUESTION + " text not null"
+			+ KEY_SOLUTION + " text not null"
+			+ KEY_MISTAKES + " number of mistakes"	
+			+");";
+	
+	private SQLiteHelper sqLiteHelper;
+	private SQLiteDatabase sqLiteDatabase;
+	
+	private Context libraryname;
+	
+	public Word(Context c){
+		libraryname=c;
+	}
+	
+	
+	public Word openToRead() throws android.database.SQLException {
+		sqLiteHelper = new SQLiteHelper(libraryname, MYDATABASE_NAME, null, MYDATABASE_VERSION);
+		sqLiteDatabase = sqLiteHelper.getReadableDatabase();
+		return this;
+	}
+
+	public Word openToWrite() throws android.database.SQLException {
+		sqLiteHelper = new SQLiteHelper(libraryname, MYDATABASE_NAME, null, MYDATABASE_VERSION);
+		sqLiteDatabase = sqLiteHelper.getWritableDatabase();
+		return this;
+	}
+
+	public void close() {
+		sqLiteHelper.close();
+	}
+
+	public long insert(String quest, String sol, short mist ) {
+
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(KEY_QUESTION, quest);
+		contentValues.put(KEY_SOLUTION, sol);
+		contentValues.put(KEY_MISTAKES, mist);
+		return sqLiteDatabase.insert(MYDATABASE_LIBRARY, null, contentValues);
+	}
+	
+	public int deleteAll() {
+		return sqLiteDatabase.delete(MYDATABASE_LIBRARY, null, null);
+	}
+	
+	public Cursor queueAll() {
+		String[] columns = new String[] { KEY_ID, KEY_QUESTION,KEY_SOLUTION,KEY_MISTAKES };
+		Cursor cursor = sqLiteDatabase.query(MYDATABASE_LIBRARY, null, null,
+				null, null, null, null);
+
+		return cursor;
+	}
+	/*public static final String KEY_STRING = "question";
+	public static final String KEY_STRING = "answer";
+	public static final */
+	public class SQLiteHelper extends SQLiteOpenHelper {
+
+		public SQLiteHelper(Context context, String name,
+				CursorFactory factory, int version) {
+			super(context, name, factory, version);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			// TODO Auto-generated method stub
+			db.execSQL(SCRIPT_CREATE_DATABASE);
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+}
