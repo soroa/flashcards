77637874ff775a0a1eceec919e9858b918260d8e
Michael <purgham@student.ethz.ch>
2013-05-21 17:20:53 +0200

    for sora: Added Arrows

--- FlashCards/src/com/example/flashcards/user_interface/Card.java
+++ FlashCards/src/com/example/flashcards/user_interface/Card.java
@@ -1,6 +1,5 @@
 package com.example.flashcards.user_interface;
 
-import android.R.string;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,13 +7,13 @@ import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.Menu;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.flashcards.R;
 import com.example.flashcards.database.Flashcard_struct;
 import com.example.flashcards.logic.MainLogic;
-
 
 public class Card extends Activity {
 
@@ -28,20 +27,30 @@ public class Card extends Activity {
 	private int side;
 	private TextView view;
 	private Flashcard_struct currentCard;
+	private ImageView arrow_right_handside;
+	private ImageView arrow_left_handside;
+	private TextView show_answer;
+	private TextView show_question;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
-
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_card);
+		arrow_left_handside = (ImageView) findViewById(R.id.right);
+		arrow_right_handside = (ImageView) findViewById(R.id.left);
+		show_answer = (TextView) findViewById(R.id.answer);
+		show_question = (TextView) findViewById(R.id.question);
+		arrow_left_handside.setVisibility(View.INVISIBLE);
+		show_question.setVisibility(View.INVISIBLE);
+
 		Intent i = getIntent();
 		Bundle b = i.getExtras();
 		myLogic = (MainLogic) b.getSerializable("myLogic");
 		currentCard = myLogic.nextCard();
 		view = new TextView(this);
 		view = (TextView) findViewById((R.id.Card_text));
-		
+
 		view.setText(currentCard.question);
 		view.setTextSize(30);
 		side = 1;
@@ -64,7 +73,7 @@ public class Card extends Activity {
 			// TODO Auto-generated method stub
 			// right to left
 			if ((e1.getX() - e2.getX()) > sensitvity) {
-				
+
 				showBack();
 				swipe_h = 0;// left
 			} else if ((e2.getX() - e1.getX()) > sensitvity) {
@@ -81,7 +90,6 @@ public class Card extends Activity {
 				myLogic.correctAnswer(false);
 				currentCard = myLogic.nextCard();
 				nextCard();
-				
 
 			} else if ((e2.getY() - e1.getY()) > sensitvity) {
 				swipe_v = 1;// up:guessed right
@@ -113,32 +121,47 @@ public class Card extends Activity {
 		 * that when the user presses back we go straight back to the deck
 		 * startActivity(i);
 		 */
-		if(side==-1) return;
+		show_question.setVisibility(View.VISIBLE);
+		arrow_left_handside.setVisibility(View.VISIBLE);
+		show_answer.setVisibility(View.INVISIBLE);
+		arrow_right_handside.setVisibility(View.INVISIBLE);
+		
+		if (side == -1)
+			return;
 		view.setText(currentCard.solution);
 		view.setTextSize(30);
-		side= -1;
+		side = -1;
 	}
-	
-	public void showFront(){
-		if(side==1) return;
+
+	public void showFront() {
+		if (side == 1)
+			return;
 		view.setText(currentCard.question);
 		view.setTextSize(30);
-		side=1;
+		show_question.setVisibility(View.INVISIBLE);
+		arrow_left_handside.setVisibility(View.INVISIBLE);
+		show_answer.setVisibility(View.VISIBLE);
+		arrow_right_handside.setVisibility(View.VISIBLE);
+		side = 1;
 	}
 
 	/*
 	 * Calls the next card
 	 */
 	public void nextCard() {
-	/*if(next==null) {
-			Toast.makeText(this,"You've reached ", Toast.LENGTH_LONG).show();
-			
-			finish();
-		}*/
-		side = 1; 
+		/*
+		 * if(next==null) { Toast.makeText(this,"You've reached ",
+		 * Toast.LENGTH_LONG).show();
+		 * 
+		 * finish(); }
+		 */
+		side = 1;
+		show_question.setVisibility(View.INVISIBLE);
+		arrow_left_handside.setVisibility(View.INVISIBLE);
+		show_answer.setVisibility(View.VISIBLE);
+		arrow_right_handside.setVisibility(View.VISIBLE);
 		view.setText(currentCard.question);
 		view.setTextSize(30);
-
 
 	}
 
